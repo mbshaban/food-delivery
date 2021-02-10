d1d855ad76d4c5a9d944143b987f9d340b4358b8
joya <joya.abn@gmail.com>
2021-02-10 12:46:05 +0430

    changes to users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('phone');
-            $table->string('account_type');
+            $table->string('account_type')->default('customer');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
