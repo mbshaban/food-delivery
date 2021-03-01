09a50aaf400248a38236180f4f76190aed33e5b6
joya <joya.abn@gmail.com>
2021-03-01 11:51:34 +0430

    some small changes

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\CategoriesController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SellersController;
+use App\Http\Controllers\CustomersController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -28,6 +29,17 @@ Route::prefix('dashboard/sellers')->group(function () {
 	    $mime = \File::mimeType($path);
 	    return \Response::make($image, 200)->header('Content-Type', $mime);
 	});
+});
+
+Route::prefix('dashboard/customers')->group(function () {
+    Route::get('/', [CustomersController::class, 'index']);
+    Route::post('/insert', [CustomersController::class, 'insert']);
+    Route::get('/profile/{file_name}', function ($filename) {
+        $path = storage_path('app') . '/profile/' . $filename;
+        $image = \File::get($path);
+        $mime = \File::mimeType($path);
+        return \Response::make($image, 200)->header('Content-Type', $mime);
+    });
 });
 
 Route::get('dashboard/products/insert', function (){
