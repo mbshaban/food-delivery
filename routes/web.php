22ffadfe7b33c1d05a31706fd6956499a5ad912d
mbshaban <m.belal.shaban@gmail.com>
2021-03-01 11:34:16 +0430

    authentication

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('dashboard', function () {
     return view('dashboard.index');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
